1f0dc2a91d106d7aa417e534f6a3ad297baa5370
Raj <rajdristant007@gmail.com>
2023-03-27 12:36:48 +0530

    navbar completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,14 +6,17 @@ import { Navbar, Sidebar } from './components';
 function App() {
   return (
     <div className='relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row'>
-      <div className='sm:flex hiden mr-10'>
+      <div className='sm:flex hidden mr-10 relative'>
         <Sidebar />
       </div>
       <div className='flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5'>
         <Navbar />
 
         <Routes>
-          <Route path="/" element={<Home />}></Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/create-campaign" element={<CreateCampaign />} />
+          <Route path="/create-details/:id" element={<CampaignDetails />} />
         </Routes>
       </div>
     </div>
